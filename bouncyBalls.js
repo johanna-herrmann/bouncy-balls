21918f221a752024d67b1c001ee9c456112d7b94
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-06-04 00:45:52 +0200

    refactor: improves clean code and code structure

--- bouncyBalls.js
+++ bouncyBalls.js
@@ -1,7 +1,76 @@
-function getRandomNumberInRange (min, max) {
-	const diff = max - min;
-	return Math.round(Math.random() * diff) + min;
+const width = window.innerWidth;
+const height = window.innerHeight;
+const container = 'container';
+
+const stage = new Konva.Stage({
+	container,
+	width,
+	height
+});
+
+const staticLayer = new Konva.Layer();
+const dynamicLayer = new Konva.Layer({ clearBeforeDraw: false });
+stage.add(staticLayer, dynamicLayer);
+
+const dynamicBackground = new Konva.Rect({ x: 0, y: 0, width, height, fill: 'rgba(0, 0, 0, 0.25)' });
+const staticBackground = new Konva.Rect({ x: 0, y: 0, width, height, fill: 'black' });
+dynamicLayer.add(dynamicBackground);
+staticLayer.add(staticBackground);
+
+const balls = [];
+
+const updateLoop = new Konva.Animation(update, dynamicLayer);
+
+updateLoop.start();
+
+stage.on('click', spawnBall);
+stage.on('touchstart', spawnBall);
+
+function spawnBall () {
+	balls.push(new Ball());
+	document.querySelector('#how-to').style.display = 'none';
 }
+
+function Ball () {
+	this.radius = getRandomNumberInRange(10, 20);
+	const x = getRandomNumberInRange(this.radius, width-this.radius);
+	const y = getRandomNumberInRange(this.radius, height-this.radius);
+	const color = getRandomColor();
+	const { velocityX, velocityY } = getRandomVelocity();
+	this.dx = velocityX;
+	this.dy = velocityY;
+	this.shape = new Konva.Circle({ radius: this.radius, x, y, fill: color });
+	dynamicLayer.add(this.shape);
+}
+
+function update () {
+	updateBalls();
+}
+
+function updateBalls () {
+	balls.forEach((ball) => {
+		updateBall(ball);
+	});
+}
+
+function updateBall (ball) {
+	let x = ball.shape.x();
+	let y = ball.shape.y();
+	x += ball.dx;
+	y += ball.dy;
+	if (x < ball.radius || x > width - ball.radius) {
+		ball.dx = -ball.dx;
+		x += ball.dx;
+	}
+	if (y < ball.radius || y > height - ball.radius) {
+		ball.dy = -ball.dy;
+		y += ball.dy;
+	}
+	ball.shape.x(x);
+	ball.shape.y(y);
+}
+
+// Maths
 
 function getRandomColor () {
 	const saturation = getRandomNumberInRange(25, 100);
@@ -19,83 +88,8 @@ function getRandomVelocity () {
 	}
 	return { velocityX, velocityY };
 }
-		
-function Circle () {
-	this.radius = getRandomNumberInRange(10, 20);
-	const x = getRandomNumberInRange(this.radius, width-this.radius);
-	const y = getRandomNumberInRange(this.radius, height-this.radius);
-	const color = getRandomColor();
-	const { velocityX, velocityY } = getRandomVelocity();
-	this.dx = velocityX;
-	this.dy = velocityY;
-	this.shape = new Konva.Circle({ radius: this.radius, x, y, fill: color });
-	bubbles.add(this.shape);
+
+function getRandomNumberInRange (min, max) {
+	const diff = max - min;
+	return Math.round(Math.random() * diff) + min;
 }
-
-function updateCircle (circle) {
-	let x = circle.shape.x();
-	let y = circle.shape.y();
-	x += circle.dx;
-	y += circle.dy;
-	if (x < circle.radius || x > width-circle.radius) {
-		circle.dx = -circle.dx;
-		x += circle.dx;
-	}
-	if (y < circle.radius || y > height-circle.radius) {
-		circle.dy = -circle.dy;
-		y += circle.dy;
-	}
-	circle.shape.x(x);
-	circle.shape.y(y);
-}
-
-function updateCircles () {
-	circles.forEach((circle) => {
-		updateCircle(circle);
-	});
-}
-
-function update () {
-	updateCircles();
-}
-
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
-const stage = new Konva.Stage({
-	container: 'container',
-	width,
-	height
-});
-
-const background = new Konva.Layer();
-const bubbles = new Konva.Layer({ clearBeforeDraw: false });
-stage.add(background, bubbles);
-
-const bubblesBg = new Konva.Rect({ x: 0, y: 0, width, height, fill: 'rgba(0, 0, 0, 0.25)' });
-const bg = new Konva.Rect({ x: 0, y: 0, width, height, fill: 'black' });
-bubbles.add(bubblesBg);
-background.add(bg);
-
-const circles = [];
-
-stage.on('click', () => {
-	circles.push(new Circle());
-});
-
-stage.on('touchstart', () => {
-	circles.push(new Circle());
-});
-
-const anim = new Konva.Animation(update, bubbles);
-
-anim.start();
-
-document.querySelector('#container').addEventListener('click', () => {
-	document.querySelector('#how-to').style.display = 'none';
-});
-
-document.querySelector('#container').addEventListener('touchstart', () => {
-	document.querySelector('#how-to').style.display = 'none';
-});

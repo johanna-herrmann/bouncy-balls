a53b5a702b6d878dbb5c9244cf69c6f5310eec3c
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-06-06 01:58:49 +0200

    feat: spawns one or more at random or specific position

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import { Stage, Layer, Rect, Animation } from 'konva';
 import Ball from './ball.js';
 import { getRandomColor, getRandomVelocity, getRandomNumberInRange } from './random.js';
 
+const HEADER_SIZE = 50;
+
 const width = window.innerWidth;
-const height = window.innerHeight;
-const container = 'container';
+const height = window.innerHeight - HEADER_SIZE;
+const container = 'ballsArea';
 
 const stage = new Stage({
 	container,
@@ -15,7 +17,7 @@ const stage = new Stage({
 const layer = new Layer({ clearBeforeDraw: false });
 stage.add(layer);
 
-const background = new Rect({ x: 0, y: 0, width, height, fill: 'rgba(0, 0, 0, 0.05)' });
+const background = new Rect({ x: 0, y: 0, width, height, fill: 'rgba(0, 0, 0, 0.25)' });
 layer.add(background);
 
 const balls = [];
@@ -24,21 +26,44 @@ const updateLoop = new Animation(update, layer);
 
 updateLoop.start();
 
-stage.on('click', spawnBall);
-stage.on('touchstart', spawnBall);
+//stage.on('click', spawnBall);
+//stage.on('touchend', spawnBall);
+document.querySelector('#spawnButton').addEventListener('click', () => {
+	spawnBalls(getRandomNumberInRange(0, width), getRandomNumberInRange(0, height));
+});
+document.querySelector('#spawnButton').addEventListener('touchend', () => {
+	spawnBalls(getRandomNumberInRange(0, width), getRandomNumberInRange(0, height));
+});
 
-function spawnBall () {
+stage.addEventListener('click', (e) => {
+	spawnBalls(e.clientX, e.clientY);
+});
+
+function spawnBalls (x, y) {
+	let count = parseInt(document.querySelector('#numberInput').value);
+	if (count > 150) {
+		count = 150;
+	}
+	if (count < 1) {
+		count = 1;
+	}
+	document.querySelector('#numberInput').value = count;
+	while (count-- > 0) {
+		spawnBall(x, y - HEADER_SIZE);
+	}
+}
+
+function spawnBall (x, y) {
 	const radius = getRandomNumberInRange(10, 20);
 	const ball = new Ball(
 		radius,
-		getRandomNumberInRange(radius, width - radius),
-		getRandomNumberInRange(radius, width - radius),
+		Math.min(Math.max(x, radius), (width - radius)),
+		Math.min(Math.max(y, radius), (height - radius)),
 		getRandomColor(),
 		getRandomVelocity()
 	);
 	balls.push(ball);
 	ball.addToLayer(layer);
-	document.querySelector('#how-to').style.display = 'none';
 }
 
 function update () {
